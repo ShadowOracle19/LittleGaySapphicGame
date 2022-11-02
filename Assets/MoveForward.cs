56da0fbcb592aca9b656a61c55d05b195f22c759
LucasCoates <47534892+ShadowOracle19@users.noreply.github.com>
2022-11-02 08:58:05 -0400

    added urp

--- Assets/MoveForward.cs
+++ Assets/MoveForward.cs
@@ -6,9 +6,16 @@ public class MoveForward : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    public GameObject steringWheel;
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * speed; 
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        
     }
 }
