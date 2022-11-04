7aaea6c7a24c18acd2ba28b66c4212217d7c04b1
LucasCoates <47534892+ShadowOracle19@users.noreply.github.com>
2022-11-04 18:26:43 -0400

    extended road and added grass area

--- Assets/Scripts/RoadInfinite.cs
+++ Assets/Scripts/RoadInfinite.cs
@@ -8,9 +8,11 @@ public class RoadInfinite : MonoBehaviour
     [SerializeField]
     private float offsetBy;
 
+    public GameObject levelPlate;
+
     private void OnTriggerEnter(Collider other)
     {
         //whenever player enters trigger move road behind them forward
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offsetBy);
+        levelPlate.transform.position = new Vector3(levelPlate.transform.position.x, levelPlate.transform.position.y, levelPlate.transform.position.z + offsetBy);
     }
 }
